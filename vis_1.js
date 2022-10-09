418b26f5761a55921f5abbcb2185314e7f06421a
mewis00 <71074399+mewis00@users.noreply.github.com>
2022-10-09 19:49:11 +1100

    Add files via upload

---
+++ vis_1.js
@@ -0,0 +1,7 @@
+var vg_1 = "Map.json";
+vegaEmbed("#map1", vg_1).then(function(result) {  
+}).catch(console.error);
+
+var vg_2 = "map2.json";
+vegaEmbed("#map2", vg_2).then(function(result) {  
+}).catch(console.error);
